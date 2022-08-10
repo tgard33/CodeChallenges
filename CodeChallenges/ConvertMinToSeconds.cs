e26cfc41b16098c52a3e69b797688bf4780ed3d1
Tyler Gardner <tyler.gardner@maersk.com>
2022-08-10 16:02:55 -0600

    Convert Minutes to Seconds Exercise

---
+++ CodeChallenges/ConvertMinToSeconds.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdabitCodePractice.CodeChallenges
+{
+    internal class ConvertMinToSeconds
+    {
+        public static int convert(int minutes)
+        {
+            int minToSeconds = minutes * 60;
+            return minToSeconds;
+        }
+
+    }
+}
