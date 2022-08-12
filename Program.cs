1f41581b501c8f29d6cca3c04ab54ff1d9c58ba2
Tyler Gardner <tyler.gardner@maersk.com>
2022-08-12 08:29:22 -0600

    Voltage and current to power exercise.

--- Program.cs
+++ Program.cs
@@ -17,6 +17,8 @@ namespace program
             Console.WriteLine($"60 minutes converted to seconds = {ConvertMinToSeconds.convert(60)} seconds.\n");
 
             Console.WriteLine($"After 2 comes {ReturnNextNumberFromIntPassed.Addition(2)}.\n");
+
+            Console.WriteLine($"If the circuit power has a voltage of 110 and a current of 3 it has {VoltageAndCurrentToPower.CircuitPower(110, 3)} power.");
         }
     }
 }
