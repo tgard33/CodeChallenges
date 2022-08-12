0e6bd9c65ba22ab8ecf3e696acd9e32c429b3c2a
Tyler Gardner <tyler.gardner@maersk.com>
2022-08-11 18:26:22 -0600

    Return Next Number from an Integer Passed in Method Code Exercise

---
+++ CodeChallenges/ReturnNextNumberFromIntPassed.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdabitCodePractice.CodeChallenges
+{
+    internal class ReturnNextNumberFromIntPassed
+    {
+        public static int Addition(int num)
+        {
+            int passedNum = ++num;
+            return passedNum;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -15,6 +15,8 @@ namespace program
             Console.WriteLine($"The sum of 5 + 3 = {ReturnSumOfTwoNumbers.Sum(5, 3)}.\n");
 
             Console.WriteLine($"60 minutes converted to seconds = {ConvertMinToSeconds.convert(60)} seconds.\n");
+
+            Console.WriteLine($"After 2 comes {ReturnNextNumberFromIntPassed.Addition(2)}.\n");
         }
     }
 }
