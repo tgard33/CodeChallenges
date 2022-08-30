eb9b6fc15b4a48cc9fe766c658626f4079b25b21
Tyler Gardner <tyler.gardner@maersk.com>
2022-08-30 11:51:27 -0600

    Buggy Code Part 4 Exercise

---
+++ VeryEasyCodeChallenges/BuggyCodePT4.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdabitCodePractice.VeryEasyCodeChallenges
+{
+    internal class BuggyCodePT4
+    {
+        public static string greeting(string name)
+        {
+            if (name == "Mubashir")
+                return "Hello, my Love!";
+            else
+                return $"Hello, {name}!"; 
+        }
+    }
+}
